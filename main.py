1293f9f410f8d56c239089c6705972ab98baf6b2
emukperv <emukperv@tcd.ie>
2022-12-28 20:36:25 +0000

    line of enemies and use of wasd keys

--- main.py
+++ main.py
@@ -1,97 +1,114 @@
 import pygame
-import random
 
-# game set-up
+# Initalise game
 pygame.init()
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
-# Create the screen
+
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+font = pygame.font.Font('freesansbold.ttf', 32)
 
 pygame.display.set_caption("Space Invaders")
-icon = pygame.image.load('space_invaders_icon.png')
+icon = pygame.image.load('icon.png')
 pygame.display.set_icon(icon)
 
-background = pygame.image.load('background.jpg')
-
-# Player
+background_image = pygame.image.load('background.jpg')
 player_image = pygame.image.load('player.png')
+enemy_image = pygame.image.load('enemy.png')
+bullet_image = pygame.image.load('bullet.png')
+
 player_x = 370
 player_y = 480
 player_dx = 0
+player_x = 370
+player_y = 480
 
-# Enemy
-enemy_image = pygame.image.load('enemy.png')
-enemy_x = random.randint(0, 736)
-enemy_y = random.randint(50, 150)
-enemy_dx = 1
-enemy_dy = 40
-
-
-# Bullet
-bullet_image = pygame.image.load('bullet.png')
 bullet_x = 0
-bullet_y = 480
-bullet_dx = 0
-bullet_dy = 10
+bullet_y = SCREEN_HEIGHT + 10
+bullet_dy = 6
 bullet_state = "ready"
 
-# Score
+enemy_dy = 40
+enemy_list = []
+for i in range(1):
+    enemy_row = []
+    for j in range(10):
+        enemy_x = 50 + j * 50
+        enemy_y = 50 + i * 50
+        enemy_dx = 1
+        enemy_row.append([enemy_x, enemy_y, enemy_dx])
+    enemy_list.append(enemy_row)
+    
 score = 0
-font = pygame.font.Font('freesansbold.ttf', 32)
 
-# Game Over text
-over_font = pygame.font.Font('freesansbold.ttf', 64)
+game_over = False
+game_over_text = font.render("Game Over", True, (255, 255, 255))
+game_over_rect = game_over_text.get_rect()
+game_over_rect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
 
-def show_score(x, y, score, font):
-    score_text = font.render("Score: " + str(score), True, (255, 255, 255))
-    screen.blit(score_text, (x, y))
+running = True
 
-def game_over_text():
-    over_text = over_font.render("GAME OVER", True, (255, 255, 255))
-    screen.blit(over_text, (200, 250))
+# Functions
+def draw_enemy(x, y):
+    if x is not None and y is not None:
+        screen.blit(enemy_image, (x, y))
 
 def player(x, y):
     screen.blit(player_image, (x, y))
 
-def enemy(x, y):
-    screen.blit(enemy_image, (x, y))
-
 def fire_bullet(x, y):
-    global bullet_state
-    bullet_state = "fire"
     screen.blit(bullet_image, (x + 16, y + 10))
-
-def is_collision(enemy_x, enemy_y, bullet_x, bullet_y):
-    distance = ((enemy_x - bullet_x)**2 + (enemy_y - bullet_y)**2)**0.5
+    pygame.display.update()
+    
+def is_collision(enemy_x, enemy_y, player_x, player_y):
+    if enemy_x is None or enemy_y is None:
+        return False
+    distance = ((enemy_x - player_x) ** 2 + (enemy_y - player_y) ** 2) ** 0.5
     if distance < 27:
         return True
     else:
         return False
-
+    
 # Game loop
-running = True
 while running:
-    screen.fill((0, 0, 0))
-    screen.blit(background, (0, 0))
-
+    # Handle events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        # Handle keystrokes
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                player_dx = -5
-            if event.key == pygame.K_RIGHT:
-                player_dx = 5
+            # Handle keystrokes
+            if event.key == pygame.K_LEFT or event.key == pygame.K_a:
+                player_dx = -3
+            if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
+                player_dx = 3
             if event.key == pygame.K_SPACE:
-                if bullet_state == "ready":
-                    bullet_x = player_x
-                    fire_bullet(bullet_x, bullet_y)
+                bullet_x = player_x
+                bullet_y = player_y - 25
+                bullet_state = "fire"
         if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_a or event.key == pygame.K_RIGHT or event.key == pygame.K_d:
                 player_dx = 0
-         # Player movement bounds
+                
+        # Bullet movement
+    if bullet_state == "fire":
+        fire_bullet(bullet_x, bullet_y)
+        bullet_y -= bullet_dy
+        if bullet_y <= 0:
+            bullet_y = SCREEN_HEIGHT + 10
+            bullet_state = "ready"
+        # Check if bullet has hit an enemy
+        # Check for collisions
+        for i in range(len(enemy_list)):
+            for j in range(len(enemy_list[i])):
+                if enemy_list[i][j] is not None:
+                    enemy_x, enemy_y, enemy_dx = enemy_list[i][j]
+                    if is_collision(enemy_x, enemy_y, bullet_x, bullet_y):
+                        bullet_y = SCREEN_HEIGHT + 10
+                        bullet_state = "ready"
+                        enemy_list[i][j] = None
+                        score += 1
+    
+    # Player movement bounds
     player_x += player_dx
     if player_x <= 0:
         player_x = 0
@@ -99,40 +116,55 @@ while running:
         player_x = 736
     
     # Enemy movement
-    enemy_x += enemy_dx
-    if enemy_x <= 0 or enemy_x >= 736:
-        enemy_dx = -enemy_dx  # Reverse the direction of movement
-        enemy_y += enemy_dy  # Drop down a row
+    for i in range(len(enemy_list)):
+        for j in range(len(enemy_list[i])):
+            if enemy_list[i][j] is not None:
+                enemy_x, enemy_y, enemy_dx = enemy_list[i][j]
+                enemy_x += enemy_dx
+                if enemy_x <= 0 or enemy_x >= 736:
+                    enemy_dx =- enemy_dx  # Reverse the direction of movement
+                    enemy_y += enemy_dy  # Drop down a row
+                enemy_list[i][j] = [enemy_x, enemy_y, enemy_dx]
+            
+        enemy_list = [row for row in enemy_list if row]
+        enemy_list = [[enemy for enemy in row if enemy] for row in enemy_list]
+            
+        if enemy_y >= player_y:
+                game_over = True
+                break
+        if game_over:
+            break
+        
+    # Remove empty rows and None elements from enemy_list
+    for i in range(len(enemy_list)):
+        enemy_row = enemy_list[i]
+        if enemy_row is None:
+            enemy_list.remove(enemy_row)
+        else:
+            for j in range(len(enemy_row)):
+                enemy = enemy_row[j]
+                if enemy is None:
+                    enemy_row.remove(enemy)
 
+            
     
-    # Bullet movement
-    if bullet_y <= 0:
-        bullet_y = 480
-        bullet_state = "ready"
-    if bullet_state == "fire":
-        fire_bullet(bullet_x, bullet_y)
-        bullet_y -= bullet_dy
+    # Clear the screen
+    screen.blit(background_image, (0, 0))
     
-    # Collision
-    collision = is_collision(enemy_x, enemy_y, bullet_x, bullet_y)
-    if collision:
-        bullet_y = 480
-        bullet_state = "ready"
-        score += 1
-        enemy_x = random.randint(0, 736)
-        enemy_y = random.randint(50, 150)
-    
-    # Game Over
-    if enemy_y > 440:
-        for i in range(3):
-            game_over_text()
-            pygame.display.update()
-            pygame.time.delay(1000)
-        running = False
-    
+    # Draw the player and enemy ships
     player(player_x, player_y)
-    enemy(enemy_x, enemy_y)
-    show_score(10, 10, score, font)
+    for i in enemy_list:
+        for j in i:
+            enemy_x, enemy_y, enemy_dx = j
+            draw_enemy(enemy_x, enemy_y)
+    
+    # Display the score
+    score_text = font.render("Score: " + str(score), True, (255, 255, 255))
+    screen.blit(score_text, (10, 10))
+    
+    # Display the game over screen if the game is over
+    if game_over:
+        screen.blit(game_over_text, game_over_rect)
+    
+    # Update the display
     pygame.display.update()
-
-pygame.quit()
